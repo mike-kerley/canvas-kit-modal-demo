1f2c8d7ae5ada720de2ff394ff8bb43e2b720f80
Mike Kerley <mike.kerley@workday.com>
2023-10-09 21:44:38 -0700

    Update modal demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { Modal } from "@workday/canvas-kit-react/modal";
+import { PrimaryButton } from "@workday/canvas-kit-react/button";
+import { Flex, Box } from "@workday/canvas-kit-react/layout";
+
+const App = () => {
+  const handleOk = () => {
+    console.log("OK clicked");
+  };
+
+  const handleCancel = () => {
+    console.log("Cancel clicked");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Modal>
+      <Modal.Target as={PrimaryButton}>Show Modal</Modal.Target>
+      <Modal.Overlay>
+        <Modal.Card>
+          <Modal.CloseIcon aria-label="Close" />
+          <Modal.Heading>Sample Modal</Modal.Heading>
+          <Modal.Body>
+            <Box as="p" marginY="zero">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </Box>
+          </Modal.Body>
+          <Flex gap="s" padding="xxs" marginTop="xxs">
+            <Modal.CloseButton as={PrimaryButton} onClick={handleOk}>
+              OK
+            </Modal.CloseButton>
+            <Modal.CloseButton onClick={handleCancel}>Cancel</Modal.CloseButton>
+          </Flex>
+        </Modal.Card>
+      </Modal.Overlay>
+    </Modal>
   );
-}
+};
 
 export default App;
